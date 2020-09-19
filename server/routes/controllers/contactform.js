d7657adbdf2ca61726d32873598a82af90eb0a6d
Atsushi Hamada <a.hamada.biz@gmail.com>
2020-09-19 10:57:16 +0900

    Minor update

--- server/routes/controllers/contactform.js
+++ server/routes/controllers/contactform.js
@@ -1,27 +1,40 @@
-
-const config = require('../../config')
-const sgMail = require('@sendgrid/mail')
+const config = require("../../config");
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 
+exports.sendFormMessage = function (req, res) {
+  const { username, email, msg } = req.body;
 
-exports.sendFormMessage = function(req, res) {
-    const { username, email, msg } = req.body
+  if (!username || !email) {
+    return res
+      .status(422)
+      .send({
+        errors: [
+          { title: "Data missing!", detail: "Provide your name and email!" },
+        ],
+      });
+  }
 
-    if(!username || !email) {
-        return res.status(422).send({errors: [{title: "Data missing!", detail: "Provide your name and email!"}]})
-    }
+  if (!msg) {
+    return res
+      .status(422)
+      .send({
+        errors: [
+          {
+            title: "Data missing!",
+            detail: "Provide your questions or requests!",
+          },
+        ],
+      });
+  }
 
-    if(!msg) {
-        return res.status(422).send({errors: [{title: "Data missing!", detail: "Provide your questions or requests!"}]})
-    }
+  const sendMsg = {
+    to: "support@aeru.me",
+    from: email,
+    subject: "[" + username + " 様]から以下の問い合わせがきました",
+    text: msg,
+  };
+  sgMail.send(sendMsg);
 
-    const sendMsg = {
-        to: 'hamada@aeru.me',
-        from: email,
-        subject: '[' + username + ' 様]から以下の問い合わせがきました',
-        text: msg
-    }
-    sgMail.send(sendMsg);
-
-    return res.json({'Sent': true})
-}
+  return res.json({ Sent: true });
+};
