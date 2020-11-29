e3668a0294a4a0a1ff0ecc6b09ffa561bd066330
Atsushi Hamada <a.hamada.biz@gmail.com>
2020-11-29 18:57:25 +0900

    Minor update

--- src/app/rental/rental-detail/rental-detail.component.ts
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -1,112 +1,127 @@
-import { Component, OnInit, OnDestroy, Input, HostListener } from '@angular/core';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { MyOriginAuthService } from 'src/app/auth/service/auth.service';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { LoginPopupComponent } from 'src/app/auth/login-popup/login-popup.component';
-import { RentalBookingComponent } from './rental-detail-booking/rental-booking.component';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  HostListener,
+} from "@angular/core";
+import { ActivatedRoute, Router, NavigationEnd } from "@angular/router";
+import { MyOriginAuthService } from "src/app/auth/service/auth.service";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { LoginPopupComponent } from "src/app/auth/login-popup/login-popup.component";
+import { RentalBookingComponent } from "./rental-detail-booking/rental-booking.component";
 
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Rental } from '../service/rental.model';
-import { Review } from 'src/app/common/review/service/review.model';
-import { Booking } from './rental-detail-booking/services/booking.model';
-import { RentalService } from '../service/rental.service';
-import { ReviewService } from 'src/app/common/review/service/review.service';
-import { EventInput } from '@fullcalendar/core';
-import timeGridPlugin from '@fullcalendar/timegrid';
-
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Rental } from "../service/rental.model";
+import { Review } from "src/app/common/review/service/review.model";
+import { Booking } from "./rental-detail-booking/services/booking.model";
+import { RentalService } from "../service/rental.service";
+import { ReviewService } from "src/app/common/review/service/review.service";
+import { EventInput } from "@fullcalendar/core";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import Swal from "sweetalert2";
 
 @Component({
-    selector: 'app-rental-detail',
-    templateUrl: './rental-detail.component.html',
-    styleUrls: ['./rental-detail.component.scss']
+  selector: "app-rental-detail",
+  templateUrl: "./rental-detail.component.html",
+  styleUrls: ["./rental-detail.component.scss"],
 })
 export class RentalDetailComponent implements OnInit, OnDestroy {
-    currentId: string
-    rental: Rental
-    rating: number
-    reviews: Review[] = []
-    safeUrl: SafeResourceUrl
-    calendarPlugins = [timeGridPlugin]; // important!
-    calendarEvents: EventInput[] = []
-    calendarBusinessHours: EventInput[] = []
+  currentId: string;
+  rental: Rental;
+  rating: number;
+  reviews: Review[] = [];
+  safeUrl: SafeResourceUrl;
+  calendarPlugins = [timeGridPlugin]; // important!
+  calendarEvents: EventInput[] = [];
+  calendarBusinessHours: EventInput[] = [];
 
-    headerOffset: number = 75; // want to replace like DEFINE HEADER_OFFSET
+  headerOffset: number = 75; // want to replace like DEFINE HEADER_OFFSET
 
-    constructor(
-        private route: ActivatedRoute,
-        private rentalService: RentalService,
-        private reviewService: ReviewService,
-        private modalService: NgbModal,
-        public router: Router,
-        public auth: MyOriginAuthService,
-        public sanitizer: DomSanitizer,
-        ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private rentalService: RentalService,
+    private reviewService: ReviewService,
+    private modalService: NgbModal,
+    public router: Router,
+    public auth: MyOriginAuthService,
+    public sanitizer: DomSanitizer
+  ) {}
 
-    ngOnInit() {
-        let navbar = document.getElementsByTagName('nav')[0];
-            navbar.classList.add('navbar-transparent');
-        let body = document.getElementsByTagName('body')[0];
-            body.classList.add('presentation-page');
-    
-        this.route.params.subscribe(
-            (params) => {
-                this.getRental(params['rentalId'])
-            })
+  ngOnInit() {
+    let navbar = document.getElementsByTagName("nav")[0];
+    navbar.classList.add("navbar-transparent");
+    let body = document.getElementsByTagName("body")[0];
+    body.classList.add("presentation-page");
+
+    this.route.params.subscribe((params) => {
+      this.getRental(params["rentalId"]);
+    });
+  }
+
+  ngOnDestroy() {
+    let navbar = document.getElementsByTagName("nav")[0];
+    navbar.classList.remove("navbar-transparent");
+    let body = document.getElementsByTagName("body")[0];
+    body.classList.remove("presentation-page");
+    if (navbar.classList.contains("nav-up")) {
+      navbar.classList.remove("nav-up");
     }
+  }
 
-    ngOnDestroy() {
-        let navbar = document.getElementsByTagName('nav')[0];
-            navbar.classList.remove('navbar-transparent');
-        let body = document.getElementsByTagName('body')[0];
-            body.classList.remove('presentation-page');
-        if (navbar.classList.contains('nav-up')) {
-            navbar.classList.remove('nav-up');
-        }
-    }
+  isYourRental() {
+    return this.rental.user._id === this.auth.getUserId();
+  }
 
-    isYourRental() {
-        return this.rental.user._id === this.auth.getUserId()
-    }
+  getRental(rentalId: string) {
+    this.rentalService.getRentalById(rentalId).subscribe((rental: Rental) => {
+      this.rental = rental;
+      // this.getAvgRating(rental._id)
+      this.getReviews(rental._id);
+      this.getSafeUrl(rental.course1Img);
+    });
+  }
 
-    getRental(rentalId: string) {
-        this.rentalService.getRentalById(rentalId).subscribe(
-            (rental: Rental) => {
-                this.rental = rental
-                // this.getAvgRating(rental._id)
-                this.getReviews(rental._id)
-                this.getSafeUrl(rental.course1Img)
-            }
-        )
-    }
+  getSafeUrl(url: string) {
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 
-    getSafeUrl(url: string) {
-        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url)
-    }
+  getAvgRating(rentalId: string) {
+    this.reviewService.getAvgRating(rentalId).subscribe((rating: number) => {
+      this.rating = rating;
+    });
+  }
 
-    getAvgRating(rentalId: string) {
-        this.reviewService.getAvgRating(rentalId).subscribe(
-            (rating: number) => {
-                this.rating = rating
-            }
-        )
-    }
+  getReviews(rentalId: string) {
+    this.reviewService.getRentalReviews(rentalId).subscribe(
+      (reviews: Review[]) => {
+        this.reviews = reviews;
+      },
+      () => {}
+    );
+  }
 
-    getReviews(rentalId: string) {
-        this.reviewService.getRentalReviews(rentalId).subscribe(
-            (reviews: Review[]) => {
-              this.reviews = reviews
-            },
-            () => { }
-        )
-    }
+  modalOpen() {
+    this.modalService.open(LoginPopupComponent);
+  }
 
-    modalOpen() {
-        this.modalService.open(LoginPopupComponent)
-    }
+  modalBookingOpen(selectedCourseTime: number) {
+    const modalRef = this.modalService.open(RentalBookingComponent, {
+      scrollable: true,
+    });
+    modalRef.componentInstance.rental = this.rental;
+    modalRef.componentInstance.selectedCourseTime = selectedCourseTime;
+  }
 
-    modalBookingOpen(selectedCourseTime: number) {
-        const modalRef = this.modalService.open(RentalBookingComponent, { scrollable: true })
-        modalRef.componentInstance.rental = this.rental
-        modalRef.componentInstance.selectedCourseTime = selectedCourseTime
-    }
+  onLineClicked(url) {
+    Swal.fire({
+      title: "Aeru（アエル）を見た",
+      text: "とLineメッセージすると予約がスムーズです",
+      type: "info",
+      confirmButtonClass: "btn btn-primary btn-lg",
+      buttonsStyling: false,
+    }).then(() => {
+      window.open(url, "_blank");
+    });
+  }
 }
