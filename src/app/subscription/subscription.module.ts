3879e6164f1c5624db7fadeb602c828b21ab9abc
Atsushi Hamada <a.hamada.biz@gmail.com>
2020-12-03 01:15:25 +0900

    Minor fix

--- src/app/subscription/subscription.module.ts
+++ src/app/subscription/subscription.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
         component: SubscriptionLessonReporterComponent,
       },
       {
-        path: "it",
+        path: "it-consulting",
         component: SubscriptionItClassComponent,
       },
     ],
